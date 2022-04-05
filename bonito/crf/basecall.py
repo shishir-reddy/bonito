b372c004a7d51d3cd4a5fedd0cb26597659f7779
shishir-reddy <shishir1@uci.edu>
2022-04-05 16:28:35 -0700

    Update basecall.py

--- bonito/crf/basecall.py
+++ bonito/crf/basecall.py
@@ -73,13 +73,13 @@ def basecall(model, reads, chunksize=4000, overlap=100, batchsize=32, reverse=Fa
 
     print("Chunks", file=sys.stderr)
     for read in reads:
-        print((read, 0, len(read.signal)), chunksize, overlap, file=sys.stderr)
+        print((read, 0, len(read.signal)), chunk(torch.from_numpy(read.signal), chunksize, overlap), file=sys.stderr)
 
     batches = thread_iter(batchify(chunks, batchsize=batchsize))
 
-    # print("Batches", file=sys.stderr)
-    # for read, batch in batches:
-    #     print(compute_scores(model, batch, reverse=reverse), file=sys.stderr)
+    print("Batches", file=sys.stderr)
+    for read, batch in batches:
+        print(compute_scores(model, batch, reverse=reverse), file=sys.stderr)
 
     # print("Scoring", file=sys.stderr)
     scores = thread_iter(
@@ -87,18 +87,18 @@ def basecall(model, reads, chunksize=4000, overlap=100, batchsize=32, reverse=Fa
     )
     # print("Completed Scoring, scores: {}".format(scores), file=sys.stderr)
 
-    # print("Scores", file=sys.stderr)
-    # for ((read, start, end), scores) in unbatchify(scores):
-    #     print((read, stitch_results(scores, end - start, chunksize, overlap, model.stride, reverse)), file=sys.stderr)
+    print("Scores", file=sys.stderr)
+    for ((read, start, end), scores) in unbatchify(scores):
+        print((read, stitch_results(scores, end - start, chunksize, overlap, model.stride, reverse)), file=sys.stderr)
 
     results = thread_iter(
         (read, stitch_results(scores, end - start, chunksize, overlap, model.stride, reverse))
         for ((read, start, end), scores) in unbatchify(scores)
     )
 
-    # print("Results", file=sys.stderr)
-    # for read, attrs in results:
-    #     print("{} {}".format(read, attrs), file=sys.stderr)
+    print("Results", file=sys.stderr)
+    for read, attrs in results:
+        print("{} {}".format(read, attrs), file=sys.stderr)
 
     return thread_iter(
         (read, fmt(model.stride, attrs))
