5ef7a522ef5188c0ee143eb4a192625878bf5db5
shishir-reddy <shishir1@uci.edu>
2022-04-04 19:42:34 -0700

    Update basecaller.py

--- bonito/cli/basecaller.py
+++ bonito/cli/basecaller.py
@@ -31,7 +31,7 @@ def main(args):
 
     sys.stderr.write(f"> loading model {args.model_directory}\n")
     try:
-        print("Loading Model")
+        sys.stderr.write("Loading Model")
         model = load_model(
             args.model_directory,
             args.device,
