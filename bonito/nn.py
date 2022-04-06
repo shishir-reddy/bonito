e0784d70a664110e7a5e732152d4660e39d866c6
shishir-reddy <shishir1@uci.edu>
2022-04-06 10:17:34 -0700

    Update nn.py

--- bonito/nn.py
+++ bonito/nn.py
@@ -176,7 +176,7 @@ class RNNWrapper(Module):
 
     def forward(self, x):
         print(self.rnn, file=sys.stderr)
-        print(x[0], file=sys.stderr)
+        print(type(x), file=sys.stderr)
         if self.reverse: x = x.flip(0)
         y, h = self.rnn(x)
         if self.reverse: y = y.flip(0)
