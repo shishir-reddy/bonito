073e3916523eacba33df42ae452d4a11cbb74846
shishir-reddy <shishir1@uci.edu>
2022-04-06 00:05:37 -0700

    Update nn.py

--- bonito/nn.py
+++ bonito/nn.py
@@ -175,7 +175,7 @@ class RNNWrapper(Module):
         if disable_state_bias: self.disable_state_bias()
 
     def forward(self, x):
-        print(self.rnn, file=sys.stderr)
+        print(x, self.rnn, file=sys.stderr)
         if self.reverse: x = x.flip(0)
         y, h = self.rnn(x)
         if self.reverse: y = y.flip(0)
