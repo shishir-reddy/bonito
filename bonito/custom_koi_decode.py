f5aa60c736f92fb8b2391aace5fac4ed0c38cdef
shishir-reddy <shishir1@uci.edu>
2022-04-07 16:11:04 -0700

    Add custom koi support

---
+++ bonito/custom_koi_decode.py
@@ -0,0 +1,57 @@
+import torch
+from koi._runtime import lib, ffi
+from custom_koi_utils import void_ptr, empty, zeros
+
+def beam_search(scores, beam_width=32, beam_cut=100.0, scale=1.0, offset=0.0, blank_score=2.0):
+
+    # if scores.dtype != torch.float16:
+    #     raise TypeError('Expected fp16 but received %s' % scores.dtype)
+
+    assert(scores.is_contiguous())
+
+    N, T, C =  scores.shape
+
+    chunks = torch.empty((N, 4), device=scores.device, dtype=torch.int32)
+    chunks[:, 0] = torch.arange(0, T * N, T)
+    chunks[:, 2] = torch.arange(0, T * N, T)
+    chunks[:, 1] = T
+    chunks[:, 3] = 0
+    chunk_results = empty((N, 8), device=scores.device, dtype=torch.int32)
+
+    # todo: reuse scores buffer?
+    aux      = empty(N * (T + 1) * (C + 4 * beam_width), device=scores.device, dtype=torch.int8)
+    path     = zeros(N * (T + 1), device=scores.device, dtype=torch.int32)
+
+    moves    = zeros(N * T, device=scores.device, dtype=torch.int8)
+    sequence = zeros(N * T, device=scores.device, dtype=torch.int8)
+    qstring  = zeros(N * T, device=scores.device, dtype=torch.int8)
+
+    args = [
+        void_ptr(chunks),
+        chunk_results.ptr,
+        N,
+        void_ptr(scores),
+        C,
+        aux.ptr,
+        path.ptr,
+        moves.ptr,
+        ffi.NULL,
+        sequence.ptr,
+        qstring.ptr,
+        scale,
+        offset,
+        beam_width,
+        beam_cut,
+        blank_score,
+    ]
+
+    lib.host_back_guide_step(*args)
+    lib.host_beam_search_step(*args)
+    lib.host_compute_posts_step(*args)
+    lib.host_run_decode(*args)
+
+    moves = moves.data.reshape(N, -1).cpu()
+    sequence = sequence.data.reshape(N, -1).cpu()
+    qstring = qstring.data.reshape(N, -1).cpu()
+
+    return sequence, qstring, moves
---
+++ bonito/custom_koi_utils.py
@@ -0,0 +1,58 @@
+import torch
+from koi._runtime import ffi
+from collections import namedtuple
+
+
+Buffer = namedtuple('Buffer', 'data ptr')
+
+
+def void_ptr(x):
+    """
+    Return a void * for given Tensor `x`.
+    """
+
+    # Try sending tensor to CPU first
+    return ffi.cast("void *", x.to(torch.device('cpu')).data_ptr())
+    # return ffi.cast("void *", x.data_ptr())
+
+
+def empty(size, device, dtype=torch.float16):
+    """
+    Create an empty Tensor of size `size` on device `device`.
+    """
+    x = torch.empty(size, dtype=dtype, device=device)
+    return Buffer(x, void_ptr(x))
+
+
+def zeros(size, device, dtype=torch.float16):
+    """
+    Create an zeros Tensor of size `size` on device `device`.
+    """
+    x = torch.zeros(size, dtype=dtype, device=device)
+    return Buffer(x, void_ptr(x))
+
+
+def quantize_tensor(tensor, levels=256, dim=0, z=None):
+    """
+    Quantize a tensor to int8, returning the per-channel scales and the quantized tensor.
+
+    If z is provided, the floating point range used for quantisation is clipped to
+    z times the standard deviation from the mean for each channel.
+    """
+
+    fp_max = torch.abs(tensor.max(dim=dim)[0])
+    fp_min = torch.abs(tensor.min(dim=dim)[0])
+    fp_range = torch.cat((fp_min[:,None], fp_max[:,None]), 1).max(dim=1)[0] * 2
+
+    if z is not None:
+        fp_mean = tensor.mean(axis=0)
+        fp_std = tensor.std(axis=0)
+        fp_range_z = 2 * (abs(fp_mean) + fp_std * z)
+        fp_range = torch.min(fp_range, fp_range_z)
+
+    quantization_scale = levels / fp_range
+    quantization_max = (levels / 2) - 1
+    tensor_quantized  = (tensor * quantization_scale).round().clip(-quantization_max, quantization_max)
+    tensor_quantized = tensor_quantized.type(torch.int8)
+
+    return  quantization_scale.float(), tensor_quantized
