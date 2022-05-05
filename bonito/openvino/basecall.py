f382fc0da99326c02399986b91907a9e8fa8024c
shishir-reddy <shishir1@uci.edu>
2022-05-05 16:16:27 -0700

    Update basecall.py

--- bonito/openvino/basecall.py
+++ bonito/openvino/basecall.py
@@ -4,6 +4,7 @@ from bonito.crf.basecall import stitch_results
 from bonito.multiprocessing import thread_iter, thread_map
 from bonito.util import chunk, stitch, batchify, unbatchify
 
+import sys
 
 def compute_scores(model, batch):
     scores = model(batch)
@@ -27,7 +28,7 @@ def decode(x, beam_width=32, beam_cut=100.0, scale=1.0, offset=0.0, blank_score=
 
 
 def basecall(model, reads, chunksize=4000, overlap=100, batchsize=32, reverse=False):
-
+    print("openvino basecall start", file=sys.stderr)
     chunks = thread_iter(
         ((read, 0, len(read.signal)), chunk(torch.from_numpy(read.signal), chunksize, overlap))
         for read in reads
